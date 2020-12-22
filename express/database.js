56c7ec222e7e18b4f42b394542405b403e36aa64
Addis Semagn <asemagn@gmail.com>
2020-12-22 05:08:47 -0500

    typo

--- express/database.js
+++ express/database.js
@@ -73,7 +73,7 @@ class Users {
       const res = await this.usersCollection.updateOne(
         { email: email },
         {
-          $set: { emailStatus: true },
+          $set: { emailSuccess: true },
         }
       );
       return res;
